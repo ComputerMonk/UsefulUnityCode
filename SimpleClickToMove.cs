0ab6156f6abec86c6234f913c22751eb7da25bbf
ComputerMonk <90413301+ComputerMonk@users.noreply.github.com>
2022-04-27 12:28:32 -0600

    Update and rename SimpleClickToMoveSinglePlayer.cs to SimpleClickToMove.cs

--- SimpleClickToMove.cs
+++ SimpleClickToMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
-public class SimpleClickToMoveSinglePlayer: MonoBehaviour {
+public class SimpleClickToMove: MonoBehaviour {
 
 public bool Enabled = true;
 public int MovementMouseButton;
